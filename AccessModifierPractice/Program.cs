bd01632b11ac1751229863a8d1e4f28c63af32fa
Paige Miller <paige_9753@hotmail.com>
2017-07-12 10:00:09 -0400

    Final Commit

--- AccessModifierPractice/Program.cs
+++ AccessModifierPractice/Program.cs
@@ -10,6 +10,17 @@ namespace AccessModifierPractice
     {
         static void Main(string[] args)
         {
+            Boat theBoat = new Boat();
+
+            theBoat.numberOfPassengers = 345;
+
+            //Create a class cell phone and determine the:
+            //3 fields
+            //2 properties
+            //4 methods
+            //For each of the members you created, determine whether it should be public or private
+              
+
         }
     }
 }
